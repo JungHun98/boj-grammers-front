a63b9283f168d2f0f2ebf16f7e12e488c43785fc
JungHun98 <j7papa@naver.com>
2025-04-09 13:34:56 +0900

    chore: java 기본 코드 수정

--- src/utils/consts/index.ts
+++ src/utils/consts/index.ts
@@ -7,7 +7,7 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello world!");
-  }
+    }
 }`,
   cpp: `#include <iostream>
 
